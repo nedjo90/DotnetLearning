80da059fe0a52c9e0389403e1b3ba0f05dfd175d
necati han <han.necati@gmail.com>
2024-02-23 00:33:19 +0100

    Organisation of projects and folders

--- DotnetLearning/Program.cs
+++ DotnetLearning/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace DotnetLearning;
+﻿using System;
+using System.Threading.Tasks;
+namespace DotnetLearning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 40; i++)
+        {
+            var value = Random.Shared.Next();
+            Console.WriteLine(value);
+        }
+        
+        
     }
 }
